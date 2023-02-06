0e61be75fad938f0a6408bd7b4a12e7aae96c6b6
JaviDk <javicho16_mu@hotmail.com>
2023-02-06 09:35:37 -0500

    ecomerce api v2

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,17 +2,14 @@ const { Sequelize } =  require('sequelize');
 require('dotenv').config();
 
 const db = new Sequelize({
-
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
-    hots: process.env.DB_HOST,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
-    dialect: "postgres",
-    logging: false,
+    dialect: 'postgres',
+    logging: false, // la base que estamos usando
     //dialectOptions: { ssl: {require: true, rejectUnauthorized: false}}
-
-
 });
 
 module.exports = db;
